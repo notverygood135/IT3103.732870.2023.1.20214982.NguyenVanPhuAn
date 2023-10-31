c43499d1ccfc0181bcd85f61150843ea07111eca
Nguyen Van Phu An <aloistrancy2905@gmail.com>
2023-11-01 01:55:02 +0700

    Lab02

--- Lab01/ChoosingOption.java
+++ Lab01/ChoosingOption.java
@@ -1,10 +1,16 @@
-package Lab1;
+package Lab01;
 
 import javax.swing.JOptionPane;
+
+import static javax.swing.JOptionPane.YES_NO_OPTION;
+
 public class ChoosingOption {
     public static void main(String[] args) {
-        int option = JOptionPane.showConfirmDialog(null,
-                "Nguyen Van Phu An - 20214982- Do you want to change the first class ticket?");
+        String[] options = new String[] {"Yes", "No", "Maybe", "Cancel"};
+        int option = JOptionPane.showOptionDialog(null,
+                "Nguyen Van Phu An - 20214982- Do you want to change the first class ticket?", "Title",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
         JOptionPane.showMessageDialog(null, "Nguyen Van Phu An - 20214982- You've chosen: "
                 + (option==JOptionPane.YES_OPTION?"Yes":"No"));
         System.exit(0);
---
+++ Lab02/AimsProject/src/Aims.java
@@ -0,0 +1,18 @@
+public class Aims {
+    public static void main(String[] args) {
+        System.out.println("Nguyen Van Phu An 20214982");
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("DVD one", "Film", "me", 100, 4.5f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("DVD two", "Biography", "not me", 200, 5.5f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("DVD three", "Documentary", "probably me", 300, 6.5f);
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+
+        cart.cartDetails();
+        cart.addDigitalVideoDisc(dvd3);
+        cart.removeDigitalVideoDisc(dvd1);
+        cart.cartDetails();
+    }
+}
